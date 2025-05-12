9738ca0ba468e4a06b16c1be97991edba45a4048
eissasabry <46199014+eissasabry@users.noreply.github.com>
2025-05-12 10:52:40 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,13 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("study-alarm-cache").then(cache => {
       return cache.addAll([
-        "./study-alarm-pwa.html",
+        "./index.html",
         "./manifest.json",
         "./"
       ]);
     })
   );
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
